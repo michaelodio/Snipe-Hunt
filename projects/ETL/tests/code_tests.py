3ad703822d7bb7232b51bafbe147d1e935500115
Matt Stillwell <18670089+mstill3@users.noreply.github.com>
2018-06-19 11:13:19 -0400

    etl
    my simple etl code

---
+++ projects/ETL/tests/code_tests.py
@@ -0,0 +1,14 @@
+from nose.tools import *
+from ex1 import code
+
+def test_add():
+    print add(2, 3)
+
+def test_subtract():
+    print subtract(5, 2)
+
+def test_multiply():
+    print multiply(3, 4)
+
+def test_divide():
+    print divide(10, 2)
