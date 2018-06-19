27120413512381b562eac3e7fe39d0e6a7a0712c
Matt Stillwell <stillwell006@gmail.com>
2018-06-19 16:07:35 -0400

    move-and-clean

---
+++ projects/ETL/ETL/utility_tests.py
@@ -0,0 +1,9 @@
+from nose.tools import *
+from Utility import *
+
+def test_get_file_paths():
+    util = Utility()
+    util.get_file_paths("C:\Users\micha\OneDrive\Desktop\movies")
+
+def test_display_file_paths():
+    print("something")
