b427e0d056f5b4c3be65ce6de1d3f6d133f59a1a
Unknown <kyle.barley@braintrust-us.com>
2018-06-25 13:54:39 +0000

    Update to target Object Detection code
    The python script now properly called the label_image script and can take an image and run the model against it and produce the confidence results.

--- projects/ObjectDetection/ObjectDetection/TargettedObjectDetectionProcess.py
+++ projects/ObjectDetection/ObjectDetection/TargettedObjectDetectionProcess.py
@@ -1,15 +1,12 @@
-import subprocess
-
-
-
+from label_image import main as labelImage
 
 
 if __name__ == "__main__":
     graph = "/home/bt-intern2/TfModule/output_graph.pb"
     labels = "/home/bt-intern2/TfModule/output_labels.txt"
     input_layer = "Placeholder"
-    output_layer = "Final_Result"
-    input_height = 224
-    input_width = 224
-    frame = 0
-    subprocess.call(("/home/bt-intern2/French-Flag-Finder/projects/ObjectDetection/ObjectDetection/label_image.py", graph),  shell=True)
+    output_layer = "final_result"
+    input_height = str(224)
+    input_width = str(224)
+    frame = "/home/bt-intern2/Downloads/TestImages/TestImagesCar/TestCar1.jpeg"
+    labelImage(graph, labels, input_layer, output_layer, input_height, input_width, frame)
