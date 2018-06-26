d073b3388706736a2717248754910b175e1b83e0
Unknown <kyle.barley@braintrust-us.com>
2018-06-26 17:18:12 +0000

    targeted OBJD can now decode json b64 image and test it with label_img()

--- projects/ObjectDetection/ObjectDetection/TargettedObjectDetectionProcess.py
+++ projects/ObjectDetection/ObjectDetection/TargettedObjectDetectionProcess.py
@@ -1,5 +1,8 @@
 from label_image import main as labelImage
 import cv2
+import base64
+import json
+import numpy as np
 
 graph = "/home/bt-intern2/TfModule/output_graph.pb"
 labels = "/home/bt-intern2/TfModule/output_labels.txt"
@@ -7,13 +10,13 @@ input_layer = "Placeholder"
 output_layer = "final_result"
 input_height = 224   # Neccessary input_height for mobilnet model
 input_width = 224    # Neccessary input_wifth for mobilenet model
+fh = open("/home/bt-intern2/Pictures/testingB64decode.jpeg", "wb")   # used for writing decoded base64 image locally to then be tested against with label_image()
+
 
 if __name__ == "__main__":
-    cap = cv2.VideoCapture(video)    # open video in openCV
-    totalFrame = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))    # grab total frames in the video
-    for x in range(totalFrame):     # loop through all of the frames and run label_image.py script against the frames to see if the french flag is present
-        retval, frame = cap.read()     # grab the next frame
-        framePath = "/home/bt-intern2/Pictures/ObjectDetection/frame" + str(x) + ".jpeg"
-        cv2.imwrite(framePath, frame)
-        print("frame " + str(x) + ":")
-        labelImage(graph, labels, input_layer, output_layer, input_height, input_width, framePath)
+    json_data_parsed = json.loads(json_data)   # loads json data into a parsed string (back to dict)
+    frameBase64 = json_data_parsed["imageBase64"]   # extracts base64 string of image (frame)
+    frame = base64.b64decode(frameBase64)         # decodes base64 image
+    fh.write(frame)        # writes decoded image to file in order to pass filepath to label_Image script
+    fh.close()
+    labelImage(graph, labels, input_layer, output_layer, input_height, input_width, "/home/bt-intern2/Pictures/testingB64decode.jpeg")    # tests frame for targeted object
