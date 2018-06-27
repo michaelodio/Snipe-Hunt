81b6abfad0314c402401c823d08cf4b52e9c8c68
Kyle-Barley3 <kyle.barley@braintrust-us.com>
2018-06-27 15:58:03 +0000

    skeleton process good, need to fix empty file

--- projects/ObjectDetection/ObjectDetection/TargettedObjectDetectionProcess.py
+++ projects/ObjectDetection/ObjectDetection/TargettedObjectDetectionProcess.py
@@ -5,7 +5,7 @@ import base64
 import json
 import numpy as np
 import sys
-sys.path.insert(0, expanduser("~") + "/French-Flag-Finder/projects/DataTransfer/DataTransfer/")   # used to import files from other folder dir in project
+sys.path.insert(0, expanduser("~") + "/French-Flag-Finder/projects/DataTransfer/DataTransfer/")   # used to import files from other folder (DataTransfer) dir in project
 from kafka_manager import *
 
 
@@ -16,20 +16,20 @@ input_layer = "Placeholder"
 output_layer = "final_result"
 input_height = 224   # Neccessary input_height for mobilnet model
 input_width = 224    # Neccessary input_wifth for mobilenet model
-fh = open("/home/bt-intern2/Pictures/testingB64decode.jpeg", "wb")   # used for writing decoded base64 image locally to then be tested against with label_image()
+fh = open(expanduser("~") + "/Pictures/testingB64decode.jpeg", "wb")   # used for writing decoded base64 image locally to then be tested against with label_image()
 
+
+def main():
+    print("Consuming messages from 'framefeeder'\n")
+    json_data_list = Consumer.pull_jsons("framefeeder")   # pull jsons from kafka topic into list for processing
+    print("Running labelImage against all frames \n")
+    for i in range(len(json_data_list)):
+        json_data_parsed = json.loads(json_data_list[i])   # loads json data into a parsed string (back to dict)
+        frameBase64 = json_data_parsed["imageBase64"]   # extracts base64 string of image (frame)
+        frame = base64.b64decode(frameBase64)         # decodes base64 image
+        fh.write(frame)        # writes decoded image to file in order to pass filepath to label_Image script
+        labelImage(graph, labels, input_layer, output_layer, input_height, input_width, (expanduser("~") + "/Pictures/testingB64decode.jpeg"))    # tests frame for targeted object
+    fh.close()
 
 if __name__ == "__main__":
-    a = json.dumps({"FrameNum": 0.0, "imageBase64": "hddjahdbnfreweifdsdfds"})
-    b = json.dumps({"FrameNum": 1.0, "imageBase64": "dsdsdsddjahdbnfreweifdsdfds"})
-    c = json.dumps({"FrameNum": 2.0, "imageBase64": "xczxcvddjahdbnfreweifdsdfds"})
-    Producer.push_json("framefeeder", a)
-    Producer.push_json("framefeeder", b)
-    Producer.push_json("framefeeder", c)
-    json_data_list = Consumer.pull_jsons("framefeeder")   # pull jsons from kafka topic into list for processing
-    #json_data_parsed = json.loads(json_data)   # loads json data into a parsed string (back to dict)
-    #frameBase64 = json_data_parsed["imageBase64"]   # extracts base64 string of image (frame)
-    #frame = base64.b64decode(frameBase64)         # decodes base64 image
-    #fh.write(frame)        # writes decoded image to file in order to pass filepath to label_Image script
-    #fh.close()
-    #labelImage(graph, labels, input_layer, output_layer, input_height, input_width, "/home/bt-intern2/Pictures/testingB64decode.jpeg")    # tests frame for targeted object
+    main()
