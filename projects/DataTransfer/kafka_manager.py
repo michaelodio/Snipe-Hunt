ed644e11283a51af6fc8fc78fbf5e7d69388e77e
Matt Stillwell <stillwell006@gmail.com>
2018-06-27 19:59:12 +0000

    Fixed kafka-manager calls to bin error

--- projects/DataTransfer/kafka_manager.py
+++ projects/DataTransfer/kafka_manager.py
@@ -8,23 +8,37 @@ class Manager:
     def start_server():
         """ Starts kafka """
         import os
-        os.system('./start_server.sh')
+        os.system('cd ../../bin/ && ./start_server.sh')
 
     @staticmethod
     def stop_server():
         """ Stops kafka """
         import os
-        os.system('./stop_server.sh')
+        os.system('cd ../../bin/ && ./stop_server.sh')
 
     @staticmethod
     def clear_topic(topic_name):
         """ Clears the topic """
         import os
-        str = './clear_topic.sh %s' % topic_name
+        str = 'cd ../../bin/ && ./clear_topic.sh %s' % topic_name
+        os.system(str)
+        
+    @staticmethod
+    def delete_topic(topic_name):
+        """ Deletes the topic """
+        import os
+        str = 'cd ../../bin/ && ./delete_topic.sh %s' % topic_name
+        os.system(str)
+        
+    @staticmethod
+    def describe_topic(topic_name):
+        """ Describes the topic """
+        import os
+        str = 'cd ../../bin/ && ./describe_topic.sh %s' % topic_name
         os.system(str)
 
     @staticmethod
     def list_topics():
         """ Lists the topics """
         import os
-        os.system('./list_topics.sh')
+        os.system('cd ../../bin/ && ./list_topics.sh')
