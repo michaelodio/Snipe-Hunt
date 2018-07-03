e2a453085f898f3b01ef6da7e34c808b6f6deeb0
Kyle-Barley3 <kyle.barley@braintrust-us.com>
2018-07-03 17:29:35 +0000

    added to decode/encode utilities and fixed targetOJD

--- projects/ObjectDetection/TargettedObjectDetectionProcess.py
+++ projects/ObjectDetection/TargettedObjectDetectionProcess.py
@@ -24,21 +24,18 @@ input_width = 224    # Neccessary input_wifth for mobilenet model
     
 def main():
     print("\nConsuming messages from 'framefeeder'")
-    json_data_list = Consumer.pull_jsons("framefeeder")   # pull jsons from kafka topic into list for processing
+    json_data = Consumer.pull_jsons("framefeeder")   # pull jsons from kafka topic into list for processing
     print("\nRunning labelImage against all frames ")
-    for i in range(len(json_data_list)):
-        json_data_parsed = json.loads(json_data_list[i])   # loads json data into a parsed string (back to dict)
-        frameBase64 = json_data_parsed["imageBase64"]   # extracts base64 string of image (frame)
-        frame = utilities.decodeFrame(frameBase64)         # decodes base64 image
-        frameAsTensor = tf.image.decode_jpeg(frame, channels=3)   # convert frame to Tensor as string
-        print("FrameNum: " + str(i))    # display which frame is being targeted object detected against
-        confidenceStat = labelImage(graph, labels, input_layer, output_layer, input_height, input_width, frameAsTensor)    # tests frame for targeted object
-        if confidenceStat != None:     # if the target object was found within the threshold confidence, append that information to the JSON file. 
-            json_data_parsed['foundTargetWithConfidence'] = str(confidenceStat)
-            json_data_list[i] = json.dumps(json_data_parsed)
-    Utilities.storeJson(json_data_list, "../../res/frameMetadataListTargetOBJD.txt")    # Store updated metadata Jsons locally
-    Utilities.exportJson(json_data_list, "target")    # export updated Json files to kafka topic 'target'
-    objectDetection()    # continue to object detection of the frames.
+    json_data_parsed = json.loads(json_data)   # loads json data into a parsed string (back to dict)
+    frame = utilities.decodeFrame(json_data_parsed)    # take parsed string and send it to utilities to decode it from base64 
+    frameAsTensor = tf.image.decode_jpeg(frame, channels=3)   # convert frame to Tensor as string
+    print("FrameNum: " + str(i))    # display which frame is being targeted object detected against
+    confidenceStat = labelImage(graph, labels, input_layer, output_layer, input_height, input_width, frameAsTensor)    # tests frame for targeted object
+    if confidenceStat != None:     # if the target object was found within the threshold confidence, append that information to the JSON file. 
+        json_data_parsed['foundTargetWithConfidence'] = str(confidenceStat)
+        json_data = json.dumps(json_data_parsed)
+    Utilities.storeJson(json_data, "../../res/frameMetadataListTargetOBJD.txt")    # Store updated metadata Jsons locally
+    Utilities.exportJson(json_data, "target")    # export updated Json files to kafka topic 'target'
     
 
 if __name__ == "__main__":
