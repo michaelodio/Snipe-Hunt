8d1f68a548df5611fdd5035cf5d15176e09789da
Kyle-Barley3 <kyle.barley@braintrust-us.com>
2018-07-03 16:25:18 +0000

    deleted calls to other scripts, no more lists, pushing continuously to kafka, fixed utilities methods

--- projects/ETL/ETLProcess.py
+++ projects/ETL/ETLProcess.py
@@ -6,10 +6,8 @@ import base64
 import argparse
 import sys
 sys.path.insert(0, "../DataTransfer/")   # used to import files from other folder dir in project
-sys.path.insert(0, "../ObjectDetection/")   # used to import files from other folder dir in project
 sys.path.insert(0, "../Utility/")   # used to import files from other folder dir in project
 from kafka_manager import *
-from TargettedObjectDetectionProcess import main as targetedObjDet
 from utilities import *
 
 
@@ -17,8 +15,6 @@ class VideoETL(object):
     def __init__(self, videoPath):
         self.videoPath = videoPath
         self.videoName = ntpath.basename(self.videoPath)
-        self.videoMetadataJson = None
-        self.frameMetadataJsonList = []
         self.totalFrame = None
         self.FPS = None
         self.videoDuration = None
@@ -29,7 +25,6 @@ class VideoETL(object):
         self.totalFrame = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))    # grab total frames in the video
         self.FPS = int(cap.get(cv2.CAP_PROP_FPS))     # grab the frames per second of the video
         self.videoDuration = round(self.totalFrame / self.FPS)   # calculate the video's duration
-        self.videoMetadataJson = json.dumps({'videoPath': self.videoPath, 'videoName': self.videoName, 'videoDuration': str(self.videoDuration) + " seconds", 'totalFrames': self.totalFrame, 'FPS': self.FPS})  # convert metadata to json
         if cap.isOpened is False:
             print("Error opening video stream or file")
         for x in range(self.totalFrame):     # loop through all of the frames and extract meta data on each frame
@@ -38,15 +33,15 @@ class VideoETL(object):
             cv2.imencode(".jpeg", videoframe)    # convert frame to JPEG image.
             frame = cv2.resize(videoframe, (300,300))    # resize frame to 300x300
             self.extractFrameMetadata(frame, frameNum, cap)    # collect metadata on the frame
-            if x > 0:    # display percent progress for splitting video frames
-                Utilities.runProgressbar((x / self.totalFrame) * 100)
         cap.release()
 
     def extractFrameMetadata(self, frame, frameNum, cap):
         timeStamp = round(cap.get(cv2.CAP_PROP_POS_MSEC)) / 1000    # collect time stamp and convert it to seconds
         retval, frameConvertedToJPG = cv2.imencode('.jpg', frame)   # encode frame to .jpg for base64string conversion
         frameAsBase64String = base64.b64encode(frameConvertedToJPG)    # encode frame to base64String
-        self.frameMetadataJsonList.append(json.dumps({'frameNum': frameNum, 'timeStamp': str(timeStamp) + " seconds", 'imageBase64': frameAsBase64String}))    # convert collected frame metadata to json and add it to list
+        frameJson = json.dumps({'videoPath': self.videoPath, 'videoName': self.videoName, 'videoDuration': str(self.videoDuration) + " seconds", 'totalFrames': self.totalFrame, 'FPS': self.FPS, 'frameNum': frameNum, 'timeStamp': str(timeStamp) + " seconds", 'imageBase64': frameAsBase64String})     # create frame json with collected metadata
+        Utilities.exportJson(frameJson, "framefeeder")    # export frame json to kafka topic
+        Utilities.storeJson(frameJson, "../../res/ETLFramesMetadata.txt")  # store frame json locally
         return
 
 def main(*positional_parameters, **keyword_parameters):
@@ -58,11 +53,6 @@ def main(*positional_parameters, **keyword_parameters):
     if FLAGS.video:
         videoEditor = VideoETL(FLAGS.video)   # create instance of VideoEditor object for processing the video with OpenCV
         videoEditor.splitFrames()   # splits the frames of the video as well as runs the extractFrameMetadata method on that frame.
-    file1 = open('../../res/videoJson.txt', 'w')   # open file handler for videoJson.txt
-    file1.write(videoEditor.videoMetadataJson)     # write the metadata Json information on the video to the file
-    Utilities.storeJson(videoEditor.frameMetadataJsonList, "../../res/frameMetadataListETL.txt")
-    Utilities.exportJson(videoEditor.frameMetadataJsonList, "framefeeder")
-    targetedObjDet()               # run targeted object detection script now that json files (frames) are pushed to kafka
           
         
         
--- projects/Utility/utilities.py
+++ projects/Utility/utilities.py
@@ -11,29 +11,18 @@ from kafka_manager import *
 
 class Utilities(object):
     
-    @staticmethod
-    def runProgressbar(percent):
-        sys.stdout.write("\r%d%%" % percent)
-        sys.stdout.flush()
         
-        
-
     @staticmethod
-    def storeJson(frameMetadataJsonList, filePath):
-        print("\nStoring metadata Json files locally... ")
-        file1 = open(filePath, 'w')   # open file handler for specific filePath
-        for i in range(len(frameMetadataJsonList)):     # loop through the list of the frames metadata Jsons and write them to the the file
-            file1.write(str(frameMetadataJsonList[i]) + "\n\n")
+    def storeJson(frameJson, filePath):
+        file1 = open(filePath, 'a+')   # open file handler for specific filePath
+        file1.write(str(frameJson) + "\n\n")
         file1.close()
 
     
     @staticmethod
-    def exportJson(frameMetadataJsonList, topic):
-        print("\nExporting metadata Json files to kafka topic: " + str(topic) + "... ")
-        for i in range(int(len(frameMetadataJsonList))):     # loop through the list of the frames metadata Jsons and push to specific kafka topic
-            Producer.push_json(topic, frameMetadataJsonList[i]) 
-            Utilities.runProgressbar((i / len(frameMetadataJsonList)) * 100)
+    def exportJson(frameJson, topic):
+            Producer.push_json(topic, frameJson) # Push frame json to specified Kafka topic
             
-            
+    
     
     
