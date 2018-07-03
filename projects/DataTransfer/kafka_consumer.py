09b88f105a4bced893e733f4c863eb47e94cb538
Kyle-Barley3 <kyle.barley@braintrust-us.com>
2018-07-03 18:13:47 +0000

    fixed utilities encode/decode and working towards new kafka structure

--- projects/DataTransfer/kafka_consumer.py
+++ projects/DataTransfer/kafka_consumer.py
@@ -9,12 +9,10 @@ class Consumer(object):
     @staticmethod
     def pull_jsons(topic_name):
         """ Listens on a topic for dictionaries """
-        json_list = []
         
         # Sets up the kafka consumer to listen for serialized json files on a specific topic
         consumer = KafkaConsumer(topic_name, value_deserializer=lambda m: json.loads(m.decode('utf-8')),
-                                 auto_offset_reset='earliest', enable_auto_commit=False, consumer_timeout_ms=1000)
+                                 enable_auto_commit=False)
         for message in consumer:
             data = message.value
-            json_list.append(data)
-        return json_list
+            return data
--- projects/ObjectDetection/TargettedObjectDetectionProcess.py
+++ projects/ObjectDetection/TargettedObjectDetectionProcess.py
@@ -24,19 +24,24 @@ input_width = 224    # Neccessary input_wifth for mobilenet model
     
 def main():
     print("\nConsuming messages from 'framefeeder'")
-    json_data = Consumer.pull_jsons("framefeeder")   # pull jsons from kafka topic into list for processing
-    print("\nRunning labelImage against all frames ")
-    json_data_parsed = json.loads(json_data)   # loads json data into a parsed string (back to dict)
-    frame = utilities.decodeFrame(json_data_parsed)    # take parsed string and send it to utilities to decode it from base64 
-    frameAsTensor = tf.image.decode_jpeg(frame, channels=3)   # convert frame to Tensor as string
-    print("FrameNum: " + str(i))    # display which frame is being targeted object detected against
-    confidenceStat = labelImage(graph, labels, input_layer, output_layer, input_height, input_width, frameAsTensor)    # tests frame for targeted object
-    if confidenceStat != None:     # if the target object was found within the threshold confidence, append that information to the JSON file. 
-        json_data_parsed['foundTargetWithConfidence'] = str(confidenceStat)
-        json_data = json.dumps(json_data_parsed)
-    Utilities.storeJson(json_data, "../../res/frameMetadataListTargetOBJD.txt")    # Store updated metadata Jsons locally
-    Utilities.exportJson(json_data, "target")    # export updated Json files to kafka topic 'target'
-    
+    consumer = Consumer()
+    json_data = consumer.pull_jsons("framefeeder")   # pull jsons from kafka topic into list for processing
+    while json_data != None:
+        json_data_parsed = json.loads(json_data)   # loads json data into a parsed string (back to dict)
+        frame = Utilities.decodeFrame(json_data_parsed)    # take parsed string and send it to utilities to decode it from base64 
+        frameAsTensor = tf.image.decode_jpeg(frame, channels=3)   # convert frame to Tensor as string
+        print("running labelImage against a frame\n")
+        confidenceStat = labelImage(graph, labels, input_layer, output_layer, input_height, input_width, frameAsTensor)    # tests frame for targeted object
+        if confidenceStat != None:     # if the target object was found within the threshold confidence, append that information to the JSON file. 
+            json_data_parsed['foundTargetWithConfidence'] = str(confidenceStat)
+            json_data = json.dumps(json_data_parsed)
+        Utilities.storeJson(json_data, "../../res/frameMetadataListTargetOBJD.txt")    # Store updated metadata Jsons locally
+        print("exporting json")
+        Utilities.exportJson(json_data, "target2")    # export updated Json files to kafka topic 'target'
+        print(json_data_parsed['frameNum'])   # print frame number that was just processed
+        print("grabbing new json...")
+        json_data = consumer.pull_jsons("framefeeder")   # pull jsons from kafka topic into list for processing
+        
 
 if __name__ == "__main__":
     main()
