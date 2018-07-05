9ceb4762d458fdae7633ff5006f3726865795949
Kyle-Barley3 <kyle.barley@braintrust-us.com>
2018-07-05 16:47:48 +0000

    added argparse to target OBJDand updated launch script accordingly

--- bin/launch.sh
+++ bin/launch.sh
@@ -5,10 +5,10 @@ echo Kafka Server Started
 ./create_topic.sh general
 ./create_topic.sh target2
 ./create_topic.sh framefeeder
+echo Topics Created
 ./clear_topic.sh general
 ./clear_topic.sh target2
 ./clear_topic.sh framefeeder
-echo Topics Created
 echo Topics Cleared
 
 cd ../projects/ObjectDetection/
@@ -16,7 +16,7 @@ python FrameLabeling.py &
 
 python GeneralObjDetection.py &
 
-python TargettedObjectDetectionProcess.py &
+python TargettedObjectDetectionProcess.py --graph "../../res/TfModel/output_graph.pb" --labels "../../res/TfModel/output_labels.txt" --input_layer "Placeholder" --output_layer "final_result" --input_height 224 --input_width 224 &
 
 cd ../ETL/
 python ETLProcess.py --video "../../res/vid.mp4"
