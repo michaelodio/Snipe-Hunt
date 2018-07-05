8c7c680e899d507a28a41f11c714d63e53a2bc48
Kyle-Barley3 <kyle.barley@braintrust-us.com>
2018-07-05 15:50:37 +0000

    added clear_topics to launch script

--- bin/launch.sh
+++ bin/launch.sh
@@ -5,7 +5,11 @@ echo Kafka Server Started
 ./create_topic.sh general
 ./create_topic.sh target2
 ./create_topic.sh framefeeder
+./clear_topic.sh general
+./clear_topic.sh target2
+./clear_topic.sh framefeeder
 echo Topics Created
+echo Topics Cleared
 
 cd ../projects/ObjectDetection/
 python FrameLabeling.py &
