3b78300b6968ad4a8c9a3683ab4f33d377cf2026
mstill3 <stillwell006@gmail.com>
2018-07-13 19:42:44 +0000

    Changed consumer to have OOD OOD style, updated run.py to include more json parsing

--- projects/DataTransfer/kafka_consumer.py
+++ projects/DataTransfer/kafka_consumer.py
@@ -8,6 +8,7 @@ class Consumer(object):
 
     @staticmethod
     def initialize(topic_name):
+        """ Initializes the consumer """
         # Sets up the kafka consumer to listen for serialized json files on a specific topic
         return KafkaConsumer(topic_name, value_deserializer=lambda m: json.loads(m.decode('utf-8')),
                                  enable_auto_commit=False)
